3e02bf96be8ea3d4cc95fbbb89844d80dfcb8c3b
Lubega Mark <lubegamark@gmail.com>
2019-10-19 16:22:56 +0300

    Determine wether loan is active or not

--- algorithms/models.py
+++ algorithms/models.py
@@ -11,14 +11,33 @@ class Account():
 
 class Loan():
 
-    def __init__(self, amount, start_date, daily_rate):
+    def __init__(self, amount, start_date, daily_rate, loan_payments):
         self.amount = amount
         self.start_date = start_date
         self.daily_rate = daily_rate
+        self.loan_payments = loan_payments
 
     @property
     def days_to_start(self):
         return self.start_date - datetime.datetime.now().date()
+
+    @property
+    def loan_payments_total(self):
+        total = 0
+        for loan_payment in self.loan_payments:
+            total += loan_payment.amount
+        return total
+
+    @property
+    def fully_paid(self):
+        return True if self.loan_payments_total >= self.amount else False
+
+    @property
+    def active(self):
+        if (self.start_date > datetime.datetime.now().date() and
+                self.fully_paid is False):
+            return True
+        return False
 
 
 class Device():
