995a02c02424b219aef4505db93c190117a1cfd2
Lubega Mark <lubegamark@gmail.com>
2019-10-20 15:44:20 +0300

    Add Tests

--- algorithms/tests.py
+++ algorithms/tests.py
@@ -1,3 +1,16 @@
-# from django.test import TestCase
+from django.test import TestCase
+from algorithms import models
 
-# Create your tests here.
+
+class TestScenarios(TestCase):
+
+    def test_scenario_1(self):
+        days_of_power = models.get_days_of_power_3_loans(
+            R1=1000,
+            D1=3,
+            R2=500,
+            D2=10,
+            R3=1500,
+            D3=7,
+            K=21000)
+        self.assertEqual(days_of_power, 10)
