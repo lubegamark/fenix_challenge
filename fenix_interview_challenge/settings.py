fb8f4ad1945125b07dc09e639df2a4c9b5d6a162
Lubega Mark <lubegamark@gmail.com>
2019-10-21 18:05:53 +0300

    Add Celery and Redis configurations

--- fenix_interview_challenge/settings.py
+++ fenix_interview_challenge/settings.py
@@ -122,3 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# CELERY SETTINGS
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Nairobi'
