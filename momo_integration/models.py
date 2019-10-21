6221b9e57ceb6034b6792dfbe070cb235599614c
Lubega Mark <lubegamark@gmail.com>
2019-10-21 15:57:00 +0300

    Add momo request model

--- momo_integration/models.py
+++ momo_integration/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class MomoRequest(models.Model):
+    PENDING = 'PENDING'
+    SUCCESSFUL = 'SUCCESSFUL'
+    FAILED = 'FAILED'
+    STATUS_CHOICES = (
+        (PENDING, PENDING),
+        (SUCCESSFUL, SUCCESSFUL),
+        (FAILED, FAILED),
+    )
+    customer = models.CharField(max_length=250,)
+    amount = models.DecimalField(decimal_places=4, max_digits=20)
+    status = models.CharField(
+        max_length=250,
+        choices=STATUS_CHOICES,
+        default=PENDING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_edit_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        """Initialize a momo payment after saving
+        """
+        super().save(*args, **kwargs)
+        make_momo_collection_request()
+
+
+def make_momo_collection_request():
+    pass
