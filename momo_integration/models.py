d1171214db067b363c6b033f9be4055e59cd35ad
Lubega Mark <lubegamark@gmail.com>
2019-10-21 16:27:47 +0300

    Add method for getting token

--- momo_integration/models.py
+++ momo_integration/models.py
@@ -28,3 +28,36 @@ class MomoRequest(models.Model):
 
 def make_momo_collection_request():
     pass
+
+
+def get_access_token():
+
+    api_base_url = os.environ.get('MOMOPAY_BASE_URL')
+    api_user_id = os.environ.get('MOMOPAY_USER_ID')
+    subscription_key = os.environ.get('MOMOPAY_SUBSCRIPTION_PRIMARY_KEY')
+    api_secret_key = os.environ.get('MOMOPAY_API_SECRET')
+
+    auth=HTTPBasicAuth(
+        api_user_id,
+        api_secret_key)
+
+    headers = {
+        'X-Reference-Id': api_user_id,
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': subscription_key,
+        }
+
+    token_url = "{}{}".format(api_base_url,'collection/token/')
+
+    r = requests.post(
+        url=token_url,
+        headers=headers,
+        data=json.dumps({}),
+        auth=auth,
+        verify=True)
+
+    print(r.status_code)
+    response_data = json.loads(r.text)
+    print(response_data)
+
+    return response_data["access_token"]
